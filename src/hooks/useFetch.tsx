4bfc30d46fa61e6d4c79c44aee17aae7337d858d
Alexander Tseng <xanderyst@gmail.com>
2025-03-16 05:37:28 -0700

    revert useFetch changes

--- src/hooks/useFetch.tsx
+++ src/hooks/useFetch.tsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 
-function useFetch<T>(url: string | null,) {
+function useFetch<T>(url: string | null) {
   const [loading, setLoading] = useState<boolean>(false);
   const [data, setData] = useState<T | null>(null);
   const [error, setError] = useState<Error | null>(null);
@@ -11,12 +11,7 @@ function useFetch<T>(url: string | null,) {
     const fetchData = async () => {
       try {
         setLoading(true);
-        const response = await fetch(url,
-          {
-            method: 'GET',
-            headers: {'Access-Control-Allow-Origin': 'https://itunes.apple.com'}
-          }
-        );
+        const response = await fetch(url);
         if (!response.ok) {
           throw new Error(`HTTP error! Status: ${response.status}`);
         }
