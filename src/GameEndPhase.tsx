754b321e6021ba2138ac50502169d21bbffe114c
Alexander Tseng <xanderyst@gmail.com>
2025-03-17 18:51:15 -0700

    Add answer in endphase

--- src/GameEndPhase.tsx
+++ src/GameEndPhase.tsx
@@ -18,9 +18,12 @@ export default function GameEndPhase({ randomSong, endGameMessage, selectRandomS
 
     return (
         <div>
+            <div className="flex justify-center items-center pb-3 pt-3 m-[4px] bg-purple-100 rounded-xl">
+                <p className="font-bold mr-2">Answer:</p>{randomSong.trackName}
+            </div>
             <AppleMusicEmbed songUrl={randomSong.trackViewUrl} />
             <div className="flex flex-col gap-4 items-center">
-                <div className="pt-10">{t(endGameMessage)}</div>
+                <div className="pt-8">{t(endGameMessage)}</div>
                 <AttemptVisualizer
                     attempts={attempts}
                     correctAnswer={randomSong.trackName}
